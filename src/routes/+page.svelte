723e09ced4d2bf4e3217c5b452a9e8e312438f9f
resqiar <resqiageng@gmail.com>
2022-11-22 18:09:21 +0700

    chore(deps): install and setup tailwind and daisy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="underline">Welcome to SvelteKit</h1>
+<button class="btn-xl btn-primary btn"> From daisy ui </button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
