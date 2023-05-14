c36a6fb3dd69d255a18c1fcf4173171c633ca7fc
resqiar <resqiageng@gmail.com>
2023-05-15 01:05:26 +0700

    chore: change hero title and desc

--- src/components/hero/MainHero.svelte
+++ src/components/hero/MainHero.svelte
@@ -1,23 +1,23 @@
 <div class="hero min-h-[600px] bg-base-200">
-	<div class="hero-content h-full w-full flex-col gap-12 py-0 px-8 lg:flex-row-reverse">
+	<div class="hero-content h-full w-full flex-col gap-12 px-8 py-0 lg:flex-row-reverse">
 		<img
 			alt="Hero"
 			src="/media/talent.png"
 			class="mr-8 max-w-sm self-end rounded-lg object-cover"
 		/>
 		<div class="max-w-[600px] pr-16">
-			<h1 class="text-5xl font-bold leading-[60px]">
-				Building digital products, brands and experience.
+			<h1 class="justify text-5xl font-bold leading-[60px]">
+				Crafting cutting-edge softwares and delivering exceptional DX/UX.
 			</h1>
 
 			<p class="py-6 leading-6">
-				a <strong>Fullstack Developer</strong> from a beautiful country of Indonesia 🇮🇩. <br /> I specialize
-				in building Web and Server development.
+				a <strong>Software Developer</strong> from a beautiful country of Indonesia 🇮🇩. <br /> I specialize
+				but not limited in building Web and Server development.
 			</p>
 
-			<div class="flex flex-row">
-				<button class="btn bg-brand-primary">Get Started</button>
-			</div>
+			<!-- <div class="flex flex-row"> -->
+			<!-- 	<button class="btn bg-brand-primary">Get Started</button> -->
+			<!-- </div> -->
 		</div>
 	</div>
 </div>
