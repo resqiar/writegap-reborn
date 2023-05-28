5697c840d17f453565d00563ca34708499c2ad90
resqiar <resqiageng@gmail.com>
2023-05-29 01:17:50 +0700

    feat: blog page now recognize the user logged in, concurrently fetch user & blog

--- src/routes/blog/+page.server.ts
+++ src/routes/blog/+page.server.ts
@@ -1,19 +1,38 @@
 import { PUBLIC_SERVER_URL } from '$env/static/public';
 import type { ServerLoadEvent } from '@sveltejs/kit';
+import type UserProfile from '../../types/UserProfile';
 
 export async function load({ fetch }: ServerLoadEvent) {
+	let user: UserProfile | null = null;
+	let blogs: any[] = [];
+	let error: boolean = false;
+
 	try {
-		const req = await fetch(`${PUBLIC_SERVER_URL}/blog/list`);
+		// Initiate fetch requests for 2 or more fetch requests concurrently,
+		// that means the JS runtime will processes these simultaniously.
+		const [userRes, blogRes] = await Promise.allSettled([
+			fetch(`${PUBLIC_SERVER_URL}/user/profile`),
+			fetch(`${PUBLIC_SERVER_URL}/blog/list`)
+		]);
 
-		const res = await req.json();
+		if (userRes.status === 'fulfilled') {
+			const { result } = await userRes.value.json();
+			user = result;
+		}
 
-		return {
-			blogs: res.result
-		};
+		if (blogRes.status === 'fulfilled') {
+			const { result } = await blogRes.value.json();
+			blogs = result;
+		} else {
+			error = true;
+		}
 	} catch (error) {
-		return {
-			blogs: null,
-			error: true
-		};
+		console.error(error);
 	}
+
+	return {
+		user: user,
+		blogs: blogs,
+		error: error
+	};
 }
--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,8 +3,17 @@
 
 	import IndexBlogBody from '../../components/body/IndexBlogBody.svelte';
 	import MainHeader from '../../components/header/MainHeader.svelte';
+	import type UserProfile from '../../types/UserProfile';
+	import { UserProfileStore } from '../../stores/UserStore';
 
 	export let data: PageData;
+
+	// Profile data derived from the SSR process
+	// @see ./+page.server.ts
+	let profile: UserProfile | null = data.user;
+
+	// update store value
+	UserProfileStore.set(profile);
 </script>
 
 <svelte:head>
