48c0c7d19fda103aa8452f580b8809b4f02de62d
resqiar <resqiageng@gmail.com>
2023-05-31 23:46:27 +0700

    chore: add ready to submit mockup

--- src/components/body/CreateBlogBody.svelte
+++ src/components/body/CreateBlogBody.svelte
@@ -25,122 +25,143 @@
 		<p>Begin your own story and let the world know.</p>
 	</div>
 
-	<div class="flex flex-col gap-4 lg:max-w-4xl">
-		<div class="divider mt-2" />
+	<section class="flex flex-col gap-8 lg:flex-row">
+		<div class="flex flex-col gap-4 lg:w-[80%]">
+			<div class="divider mt-2" />
 
-		<div>
-			<!-- DRAG DROP IMAGE -->
-			<div class="flex w-full items-center justify-between">
-				<div class="mx-1 my-2">
-					<h2 class="font-semibold">Blog Image Cover</h2>
-					<p class="text-sm">
-						The image acts as a thumbnail, therefore make it as interesting as possible.
-					</p>
+			<div>
+				<!-- DRAG DROP IMAGE -->
+				<div class="flex w-full items-center justify-between">
+					<div class="mx-1 my-2">
+						<h2 class="font-semibold">Blog Image Cover</h2>
+						<p class="text-sm">
+							The image acts as a thumbnail, therefore make it as interesting as possible.
+						</p>
+					</div>
+
+					<!-- DELETE / REVERT IMAGE -->
+					{#if preview}
+						<button
+							on:click={() => {
+								imageFile = null;
+								preview = null;
+							}}
+							title="Discard Image"
+							class="btn-sm btn-circle btn"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+								><path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M6 18L18 6M6 6l12 12"
+								/></svg
+							>
+						</button>
+					{/if}
 				</div>
 
-				<!-- DELETE / REVERT IMAGE -->
+				<!-- IMAGE PREVIEW -->
 				{#if preview}
-					<button
-						on:click={() => {
-							imageFile = null;
-							preview = null;
-						}}
-						title="Discard Image"
-						class="btn-sm btn-circle btn"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							><path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M6 18L18 6M6 6l12 12"
-							/></svg
-						>
-					</button>
+					<div>
+						<img
+							class="max-h-[500px] w-full rounded-lg object-cover"
+							src={preview.toString()}
+							alt="preview"
+						/>
+					</div>
+				{:else}
+					<!-- IMAGE FILE DROPZONE -->
+					<ImageDropzone onDropSuccess={(file) => (imageFile = file)} />
 				{/if}
 			</div>
 
-			<!-- IMAGE PREVIEW -->
-			{#if preview}
-				<div>
-					<img
-						class="max-h-[500px] w-full rounded-lg object-cover"
-						src={preview.toString()}
-						alt="preview"
-					/>
+			<!-- TITLE INPUT -->
+			<div class="form-control w-full">
+				<div class="mx-1 my-2">
+					<h2 class="font-semibold">Blog Title</h2>
 				</div>
-			{:else}
-				<!-- IMAGE FILE DROPZONE -->
-				<ImageDropzone onDropSuccess={(file) => (imageFile = file)} />
-			{/if}
-		</div>
-
-		<!-- TITLE INPUT -->
-		<div class="form-control w-full">
-			<div class="mx-1 my-2">
-				<h2 class="font-semibold">Blog Title</h2>
-			</div>
-			<input
-				id="title-input"
-				type="text"
-				placeholder="e.g The Programmer Story: Hello World"
-				class="input-bordered input w-full"
-			/>
-			<label class="label" for="title-input">
-				<span class="label-text-alt">0/70</span>
-			</label>
-		</div>
-
-		<!-- SUMMARY INPUT -->
-		<div class="form-control w-full">
-			<div class="mx-1 my-2">
-				<h2 class="font-semibold">Blog Summary</h2>
-				<p class="text-sm">Blog summary is used for short introduction into your blog.</p>
-			</div>
-			<textarea
-				class="textarea-bordered textarea"
-				placeholder="e.g The Programmer Story: Hello World follows Alex, a skilled programmer, on a transformative journey. Alex creates an AI program named Hello World that simulates human emotions. As the program evolves, Alex realizes its immense potential..."
-			/>
-		</div>
-
-		<div>
-			<div class="mx-1 my-2">
-				<h2 class="font-semibold">Blog Content</h2>
+				<input
+					id="title-input"
+					type="text"
+					placeholder="e.g The Programmer Story: Hello World"
+					class="input-bordered input w-full"
+				/>
+				<label class="label" for="title-input">
+					<span class="label-text-alt">0/70</span>
+				</label>
 			</div>
 
-			<!-- CONTENT EDITOR COMPONENT -->
-			<!-- LAZY LOAD TO SPEED UP TTI AND MINIMIZE CODE SIZE (reduce almost 50% size) -->
-			{#await import('../others/ContentEditor.svelte')}
-				<div
-					transition:fade
-					class="min-h-[300px] bg-base-300 rounded-lg flex items-center justify-center"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6 animate-bounce"
+			<!-- SUMMARY INPUT -->
+			<div class="form-control w-full">
+				<div class="mx-1 my-2">
+					<h2 class="font-semibold">Blog Summary</h2>
+					<p class="text-sm">Blog summary is used for short introduction into your blog.</p>
+				</div>
+				<textarea
+					class="textarea-bordered textarea"
+					placeholder="e.g The Programmer Story: Hello World follows Alex, a skilled programmer, on a transformative journey. Alex creates an AI program named Hello World that simulates human emotions. As the program evolves, Alex realizes its immense potential..."
+				/>
+			</div>
+
+			<div>
+				<div class="mx-1 my-2">
+					<h2 class="font-semibold">Blog Content</h2>
+				</div>
+
+				<!-- CONTENT EDITOR COMPONENT -->
+				<!-- LAZY LOAD TO SPEED UP TTI AND MINIMIZE CODE SIZE (reduce almost 50% size) -->
+				{#await import('../others/ContentEditor.svelte')}
+					<div
+						transition:fade
+						class="min-h-[300px] bg-base-300 rounded-lg flex items-center justify-center"
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
-						/>
-					</svg>
-					<p class="animate-pulse">Initializing Editor...</p>
-				</div>
-			{:then Module}
-				<div transition:fade>
-					<Module.default handleContentChange={(e) => (content = e.detail.value)} {content} />
-				</div>
-			{/await}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6 animate-bounce"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"
+							/>
+						</svg>
+						<p class="animate-pulse">Initializing Editor...</p>
+					</div>
+				{:then Module}
+					<div transition:fade>
+						<Module.default handleContentChange={(e) => (content = e.detail.value)} {content} />
+					</div>
+				{/await}
+			</div>
 		</div>
-	</div>
+
+		<div class="lg:w-[20%]">
+			<div class="flex w-full flex-col items-center justify-center rounded-lg bg-base-300 py-4">
+				<h2 class="font-semibold">Ready to Submit?</h2>
+				<p class="text-center text-sm">
+					Don't worry, you can choose to create it as draft or immediately publish the blog.
+				</p>
+
+				<div class="my-4 flex w-full flex-col gap-2 px-4">
+					<button class="btn-sm btn w-full gap-2 rounded-full text-sm font-normal normal-case">
+						Create as <span class="font-semibold text-red-300">Draft</span>
+					</button>
+
+					<button class="btn-sm btn w-full gap-2 rounded-full text-sm font-normal normal-case">
+						Create as <span class="font-semibold text-amber-300">Published</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	</section>
 </main>
