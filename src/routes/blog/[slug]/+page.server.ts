bab502bd851521ad15149c376371fe62114e6bc1
resqiar <resqiageng@gmail.com>
2023-06-04 19:54:04 +0700

    feat: add route and get dynamic blog detail. also show 404 when not found

---
+++ src/routes/blog/[slug]/+page.server.ts
@@ -0,0 +1,42 @@
+import { PUBLIC_SERVER_URL } from '$env/static/public';
+import { error, type ServerLoadEvent } from '@sveltejs/kit';
+import type UserProfile from '../../../types/UserProfile';
+
+export async function load({ fetch, params }: ServerLoadEvent) {
+	let blogID = params.slug;
+	let userProfile: UserProfile | null = null;
+
+	try {
+		const [userReq, blogReq] = await Promise.allSettled([
+			fetch(`${PUBLIC_SERVER_URL}/user/profile`),
+			fetch(`${PUBLIC_SERVER_URL}/blog/get`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({
+					id: blogID
+				})
+			})
+		]);
+
+		// if request status is not 200 (OK)
+		// throw error, the catched error then redirect to 404 page
+		if (blogReq.status === 'rejected' || !blogReq.value.ok) throw Error();
+
+		if (userReq.status === 'fulfilled' && userReq.value.ok) {
+			const res = await userReq.value.json();
+			userProfile = res.result;
+		}
+
+		// get blog result value from the fulfilled request
+		const { result } = await blogReq.value.json();
+
+		return {
+			user: userProfile,
+			blog: result
+		};
+	} catch (err) {
+		throw error(404);
+	}
+}
