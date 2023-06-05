3a627d5b92f5462ec9feffd4856d749a4b0b0c89
resqiar <resqiageng@gmail.com>
2023-06-05 17:53:02 +0700

    feat: add nprogress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
-<script>
+<script lang="ts">
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
 	import '../styles/app.css';
 	import '../styles/fonts.css';
+	import '../styles/nprogress.css';
+
+	/**
+	 * NProgress Configs
+	 * Simply show loading progress at the top
+	 * When navigating around client-side
+	 * @see https://github.com/rstacruz/nprogress#configuration
+	 */
+	NProgress.configure({
+		speed: 500,
+		minimum: 0.16
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <slot />
