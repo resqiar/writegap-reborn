bf81521232477cbd558e721d179fb7fcbad928cc
resqiar <resqiageng@gmail.com>
2023-06-09 01:39:27 +0700

    chore: show coming soon to later-updated page

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import ComingSoonBody from '../../components/body/ComingSoonBody.svelte';
+</script>
+
+<svelte:head>
+	<!-- head content -->
+	<title>Coming Soon</title>
+</svelte:head>
+
+<main>
+	<ComingSoonBody />
+</main>
