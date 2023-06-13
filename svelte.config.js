a762579edbbf1dc348e699dc32c964de40f556ed
resqiar <resqiageng@gmail.com>
2023-06-14 02:02:52 +0700

    chore: move from adapter-auto to adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +11,10 @@ const config = {
     }),
   ],
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      runtime: "edge",
+      regions: "all"
+    })
   }
 };
 
