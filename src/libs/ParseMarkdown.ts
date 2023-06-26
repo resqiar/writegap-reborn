520d78c49f5c74548b0610433d5097138065e9fa
resqiar <resqiageng@gmail.com>
2023-06-27 01:03:47 +0700

    fix: fix missing br & hr while stay safe

--- src/libs/ParseMarkdown.ts
+++ src/libs/ParseMarkdown.ts
@@ -2,36 +2,35 @@ import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
-import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import remarkGfm from 'remark-gfm';
 import rehypeHighlight from 'rehype-highlight';
+import sanitizeHtml from 'sanitize-html';
+import { allowedTags } from "../data/constants"
 
 /**
- * Compiles raw Markdown content using unified
- * along with defined remark and rehype plugins.
- *
- * @param {string} raw - The raw Markdown content to compile.
- * @returns {Promise<string>} - The compiled Markdown content.
- */
+* Compiles raw Markdown content using unified
+* along with defined remark and rehype plugins.
+*
+* @param {string} raw - The raw Markdown content to compile.
+* @returns {Promise<string>} - The compiled Markdown content.
+*/
 export default async function parseMD(raw: string): Promise<string> {
-	const parsed = unified()
-		.use(remarkParse)
-		.use(remarkRehype)
-		.use(remarkGfm) // Github Flavoured Markdown
-		.use(rehypeSanitize, {
-			...defaultSchema,
-			attributes: {
-				...defaultSchema.attributes,
-				code: [
-					...(defaultSchema.attributes?.code || []),
-					// List of all allowed languages:
-					['className']
-				]
-			}
-		})
-		.use(rehypeHighlight)
-		.use(rehypeStringify)
-		.processSync(raw);
+  const parsed = unified()
+  .use(remarkParse)
+  .use(remarkRehype, {allowDangerousHtml: true})
+  .use(remarkGfm) // Github Flavoured Markdown
+  .use(rehypeHighlight)
+  .use(rehypeStringify, {allowDangerousHtml: true})
+  .processSync(raw);
 
-	return parsed.value.toString();
+  const sanitizedDOM = sanitizeHtml(parsed.value.toString(), {
+    allowedTags: allowedTags,
+    allowedAttributes: {
+      code: ["class"],
+      span: ["class"],
+      img: [ 'src', 'alt', 'width', 'height', 'target' ]
+    },
+  });
+
+  return sanitizedDOM;
 }
