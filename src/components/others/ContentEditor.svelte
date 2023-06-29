89bfe9827f7852156f158eeb1b0be8dcd6dce599
resqiar <resqiageng@gmail.com>
2023-06-30 04:52:53 +0700

    fix: anchor links now all default to open in new tab

--- src/components/others/ContentEditor.svelte
+++ src/components/others/ContentEditor.svelte
@@ -4,16 +4,24 @@
 	// plugins
 	import gfm from '@bytemd/plugin-gfm';
 	import highlight from '@bytemd/plugin-highlight-ssr';
-	import '../../styles/highlighter/atom-one-dark.css';
+	import rehypeExternalLinks from 'rehype-external-links';
 
 	import 'bytemd/dist/index.css';
 	import '../../styles/mdeditor.css';
 	import '../../styles/custom-bytemd.css';
+	import '../../styles/highlighter/atom-one-dark.css';
 
 	export let content: string = '';
 	export let handleContentChange: (e: any) => void;
 
-	let plugins: BytemdPlugin[] = [gfm(), highlight()];
+	function customURL(): BytemdPlugin {
+		return {
+			rehype: (processor) =>
+				processor.use(rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] })
+		};
+	}
+
+	let plugins: BytemdPlugin[] = [gfm(), highlight(), customURL()];
 </script>
 
 <Editor value={content} on:change={handleContentChange} mode="auto" maxLength={50000} {plugins} />
